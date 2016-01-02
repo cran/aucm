6b65429ac3ac2eae879b6dd4d567f5cb884d93c7
Youyi Fong <youyifong@gmail.com>
2016-01-02 09:51:09 +0000

    version 2016.1-2

---
+++ src/C_dgesv.c
@@ -0,0 +1,14 @@
+#include <R.h>
+#include <R_ext/Lapack.h>
+void C_dgetrf(int* M, int* N, double* A, int* LDA, int* IPIV, int*INFO){
+	F77_CALL(dgetrf)(M, N, A, LDA, IPIV, INFO);
+}
+void C_dgetrs(int* N, int* NRHS, double* A, int* LDA, int* IPIV, double* B,int* LDB,int*INFO){
+	F77_CALL(dgetrs)("N", N, NRHS, A, LDA, IPIV, B, LDB, INFO);
+}
+void C_dgesv(int* N, int* NRHS, double* A, int* LDA, int* IPIV, double* B, int* LDB, int* INFO){
+F77_CALL(dgesv)( N, NRHS, A, LDA, IPIV, B, LDB, INFO );
+}
+
+
+
